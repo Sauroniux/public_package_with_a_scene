e12422ca1dec5f9e0752cd59a95a6a2ca3faf0f2
Augustinas-simkus <augustinas.simkus000@gmail.com>
2020-11-23 17:49:58 +0200

    Initial

---
+++ Scripts/SwitchScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+	public void Switch()
+	{
+		SceneManager.LoadScene(m_SceneToLoad);
+	}
+
+	[SerializeField]
+	private string m_SceneToLoad;
+}
